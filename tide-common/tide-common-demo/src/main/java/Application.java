05f147f7993c2e0d3a7c91c4e79eb5fc81f6db86
YangMingyang <minecraftymy@163.com>
2022-03-24 16:44:05 +0800

    1

---
+++ tide-common/tide-common-demo/src/main/java/Application.java
@@ -0,0 +1,11 @@
+import util.FruitInfoUtil;
+
+/**
+ * @author YangMingYang
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        FruitInfoUtil.getFruitInfo(Apple.class);
+    }
+}
--- tide-common/tide-common-demo/src/main/java/annotation/FruitName.java
+++ tide-common/tide-common-demo/src/main/java/annotation/FruitName.java
@@ -1,9 +1,15 @@
 package annotation;
 
+import java.lang.annotation.*;
+
 /**
  * @描述： FruitName
  * @作者： YangMingYang
  * @日期： 2022/3/24-16:15
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface FruitName {
+    String value() default "";
 }
