3b524678614943528f83adb8cb17e7dd452436d8
YangMingyang <minecraftymy@163.com>
2022-03-24 16:52:35 +0800

    1

--- tide-common/tide-common-demo/src/main/java/annotation/FruitName.java
+++ tide-common/tide-common-demo/src/main/java/annotation/FruitName.java
@@ -3,9 +3,7 @@ package annotation;
 import java.lang.annotation.*;
 
 /**
- * @描述： FruitName
- * @作者： YangMingYang
- * @日期： 2022/3/24-16:15
+ * @author YangMingYang
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
